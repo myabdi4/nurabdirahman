8b8fbf1d033c295b2e51958e31a276197bf03c40
Abdirahman Nur <myabdi4@gmail.com>
2023-08-31 03:53:37 -0500

    Update Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -4,7 +4,7 @@ const Section = () => {
       <div className="dark:bg:gray-900 lg:pt-20 pt-5">
         <div className="flex sm:justify-center space-y-5 items-center flex-col sm:text-3xl text-lg text-center">
           <h2 className="font-bold">HI, I AM ABDIRAHMAN NUR</h2>
-          <p className="pb-5">
+          <p className="pb-5 text-xl text-justice">
             A skilled front-end developer passionate about crafting captivating
             <span className="hidden sm:block sm:text-center"></span>
             -web experiences that blend design and technology seamlessly
