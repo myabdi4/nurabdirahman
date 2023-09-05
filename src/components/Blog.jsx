fdee59716674470605d3aeb6456f6c0c14d36bbf
Abdirahman Nur <myabdi4@gmail.com>
2023-09-05 00:47:23 -0500

    Update Blog.jsx

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -4,7 +4,7 @@ function Blog() {
       {" "}
       <div className="sm:p-10">
         {/* HERE ARE FOR PROJECTS */}
-        <h1 className="text-2xl ml-40 font-bold sm:text-center">LATEST BLOGS</h1>
+        <h1 className="text-2xl sm:ml-40 font-bold text-center sm:text-start">LATEST BLOGS</h1>
         <div className="sm:grid flex items-center justify-center">
           <div className="sm:grid sm:rid-col-2 justify-start sm:ml-40">
             {/* BLOG HERE 1 */}
