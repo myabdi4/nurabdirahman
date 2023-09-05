d0ac4862730bcf4ec37e44cad2ceb8e25fb1226e
Abdirahman Nur <myabdi4@gmail.com>
2023-09-05 00:45:19 -0500

    Update Blog.jsx

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -8,15 +8,15 @@ function Blog() {
         <div className="sm:grid flex items-center justify-center">
           <div className="sm:grid sm:rid-col-2 justify-start sm:ml-40">
             {/* BLOG HERE 1 */}
-            <div className="md:flex gap-5 p-4 items-center">
+            <div className="md:flex gap-5 p-3 items-center">
               <img
                 className="w-80 hover:cursor-pointer hover:shadow-lg rounded sm:ml-10"
                 src="/blog.jpeg"
                 alt=""
               />
 
-              <div className="flex flex-col gap-5 p-2 items-start">
-                <p className="text-justify  sm:w-3/4">
+              <div className="flex flex-col gap-5 pt-4 items-start">
+                <p className="sm:text-justify text-start sm:w-3/4">
                   Savor the moment at Aramo Foods, where taste and speed unite!
                   Our crave-worthy menu delivers flavor-packed burgers, crispy
                   fries, and more – all served up swiftly for your satisfaction.
