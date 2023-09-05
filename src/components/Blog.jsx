3d5014d04909b699f6922d8859b298477e631921
Abdirahman Nur <myabdi4@gmail.com>
2023-09-04 22:09:46 -0500

    view changes on small screen

---
+++ src/components/Blog.jsx
@@ -0,0 +1,45 @@
+function Blog() {
+  return (
+    <>
+      {" "}
+      <div className="sm:p-10">
+        {/* HERE ARE FOR PROJECTS */}
+        <h1 className="text-2xl ml-40 font-bold sm:text-center">LATEST BLOGS</h1>
+        <div className="sm:grid flex items-center justify-center">
+          <div className="sm:grid sm:rid-col-2 justify-start sm:ml-40 bg-red-600">
+            {/* BLOG HERE 1 */}
+            <div className="md:flex gap-5 p-4 items-center">
+              <img
+                className="w-80 hover:cursor-pointer hover:shadow-lg rounded sm:ml-10"
+                src="/blog.jpeg"
+                alt=""
+              />
+
+              <div className="flex flex-col gap-5 p-2 items-start">
+                <p className="text-justify  sm:w-3/4">
+                  Savor the moment at Aramo Foods, where taste and speed unite!
+                  Our crave-worthy menu delivers flavor-packed burgers, crispy
+                  fries, and more – all served up swiftly for your satisfaction.
+                  BiteBliss: Taste, Speed, Bliss!
+                </p>
+                <div>
+                  <a
+                    href="https://github.com/myabdi4/myabdifoods"
+                    target="_blink"
+                    className=" p-2 rounded bg-green-800 w-max"
+                  >
+                    READ MORE
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/* END HERE */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Blog;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,5 @@
+import Blog from "./Blog";
+
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
@@ -57,179 +59,12 @@ const Main = () => {
                   </div>
                 </div>
 
-                {/* CARDS HERE 1 */}
-                <div className="image flex flex-col gap-3 items-center p-4">
-                  <img
-                    className="sm:w-80 hover:cursor-pointer hover:shadow-lg object-cover rounded"
-                    src="/chicken.jpg"
-                    alt=""
-                  />
-                  <div className="flex gap-2">
-                    <a
-                      href="https://github.com/myabdi4/myabdifoods"
-                      target="_blink"
-                      className=" p-2 rounded bg-green-800 w-max"
-                    >
-                      SOURCE CODE
-                    </a>
-
-                    <a
-                      href="https://myabdifoods.netlify.app/"
-                      target="_blink"
-                      className=" p-2 rounded bg-green-800 w-max"
-                    >
-                      🌐 MYABDIFOODS.COM
-                    </a>
-                  </div>
-                </div>
-
-                {/* CARDS HERE 1 */}
-                <div className="image flex flex-col gap-3 items-center p-4">
-                  <img
-                    className="sm:w-80 hover:cursor-pointer hover:shadow-lg object-cover rounded"
-                    src="/chicken.jpg"
-                    alt=""
-                  />
-                  <div className="flex gap-2">
-                    <a
-                      href="https://github.com/myabdi4/myabdifoods"
-                      target="_blink"
-                      className=" p-2 rounded bg-green-800 w-max"
-                    >
-                      SOURCE CODE
-                    </a>
-
-                    <a
-                      href="https://myabdifoods.netlify.app/"
-                      target="_blink"
-                      className=" p-2 rounded bg-green-800 w-max"
-                    >
-                      🌐 MYABDIFOODS.COM
-                    </a>
-                  </div>
-                </div>
-                {/* CARDS HERE 1 */}
-                <div className="image flex flex-col gap-3 items-center p-4">
-                  <img
-                    className="sm:w-80 hover:cursor-pointer hover:shadow-lg object-cover rounded"
-                    src="/chicken.jpg"
-                    alt=""
-                  />
-                  <div className="flex gap-2">
-                    <a
-                      href="https://github.com/myabdi4/myabdifoods"
-                      target="_blink"
-                      className=" p-2 rounded bg-green-800 w-max"
-                    >
-                      SOURCE CODE
-                    </a>
-
-                    <a
-                      href="https://myabdifoods.netlify.app/"
-                      target="_blink"
-                      className=" p-2 rounded bg-green-800 w-max"
-                    >
-                      🌐 MYABDIFOODS.COM
-                    </a>
-                  </div>
-                </div>
-
-                {/* CARDS HERE 1 */}
-                <div className="image flex flex-col gap-3 items-center p-4">
-                  <img
-                    className="sm:w-80 hover:cursor-pointer hover:shadow-lg object-cover rounded"
-                    src="/chicken.jpg"
-                    alt=""
-                  />
-                  <div className="flex gap-2">
-                    <a
-                      href="https://github.com/myabdi4/myabdifoods"
-                      target="_blink"
-                      className=" p-2 rounded bg-green-800 w-max"
-                    >
-                      SOURCE CODE
-                    </a>
-
-                    <a
-                      href="https://myabdifoods.netlify.app/"
-                      target="_blink"
-                      className=" p-2 rounded bg-green-800 w-max"
-                    >
-                      🌐 MYABDIFOODS.COM
-                    </a>
-                  </div>
-                </div>
-
                 {/* END HERE */}
               </div>
             </div>
           </div>
 
-          <div className="sm:p-10">
-            {/* HERE ARE FOR PROJECTS */}
-            <h1 className="text-2xl ml-40 font-bold sm:text-start">
-              LATEST BLOGS
-            </h1>
-            <div className="sm:grid flex items-center justify-center">
-              <div className="sm:grid sm:rid-col-2 justify-start sm:ml-40">
-                {/* BLOG HERE 1 */}
-                <div className="sm:flex sm:items-start gap-5 p-4">
-                  <img
-                    className="w-80 hover:cursor-pointer hover:shadow-lg rounded ml-10"
-                    src="/blog.jpeg"
-                    alt=""
-                  />
-
-                  <div className="flex flex-col gap-5 p-2 items-start">
-                    <p className="text-justify  sm:w-3/4">
-                      Savor the moment at Aramo Foods, where taste and speed
-                      unite! Our crave-worthy menu delivers flavor-packed
-                      burgers, crispy fries, and more – all served up swiftly
-                      for your satisfaction. BiteBliss: Taste, Speed, Bliss!
-                    </p>
-                    <div>
-                      <a
-                        href="https://github.com/myabdi4/myabdifoods"
-                        target="_blink"
-                        className=" p-2 rounded bg-green-800 w-max"
-                      >
-                        READ MORE
-                      </a>
-                    </div>
-                  </div>
-                </div>
-
-                {/* BLOG HERE 1 */}
-                <div className="sm:flex sm:items-start gap-5 p-4">
-                  <img
-                    className="w-80 hover:cursor-pointer hover:shadow-lg rounded ml-10"
-                    src="/blog.jpeg"
-                    alt=""
-                  />
-
-                  <div className="flex flex-col gap-5 p-2 items-start">
-                    <p className="text-justify  sm:w-3/4">
-                      Savor the moment at Aramo Foods, where taste and speed
-                      unite! Our crave-worthy menu delivers flavor-packed
-                      burgers, crispy fries, and more – all served up swiftly
-                      for your satisfaction. BiteBliss: Taste, Speed, Bliss!
-                    </p>
-                    <div>
-                      <a
-                        href="https://github.com/myabdi4/myabdifoods"
-                        target="_blink"
-                        className=" p-2 rounded bg-green-800 w-max"
-                      >
-                        READ MORE
-                      </a>
-                    </div>
-                  </div>
-                </div>
-
-                {/* END HERE */}
-              </div>
-            </div>
-          </div>
+          <Blog />
         </div>
       </div>
     </>
