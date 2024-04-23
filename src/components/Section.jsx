f7959bdd69ea8501f102c06373991f9725c66fbd
Abdirahman Nur <myabdi4@gmail.com>
2024-04-23 01:20:17 -0500

    Update Section.jsx
    change few lines

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,18 +1,19 @@
 const Section = () => {
   return (
     <>
-      <div className="dark:bg:gray-900 lg:pt-20 p-2">
-        <div className="flex sm:justify-center space-y-5 items-center flex-col sm:text-3xl text-lg text-center bg-red-500 p-10">
+      <div className="dark:bg:gray-900 p-2">
+        <div className="flex-col md: flex sm:justify-center space-y-5 items-center sm:text-3xl text-lg text-center p-10">
           <div className="">
             <img
               className="rounded-full ring-2 ring-white w-64 justify-center"
               src="/profile.jpg"
               alt="logo"
             />
-            <h2 className="font-bold">HI, I AM ABDIRAHMAN NUR</h2>
+           
           </div>
           <div>
-            <p className="pb-5 text-lef text-md">
+            <h2 className="font-bold pb-5 text-center">HI, I AM ABDIRAHMAN NUR</h2>
+            <p className="text-justify text-md">
               A skilled front-end developer passionate about crafting
               captivating -web experiences that blend design and technology
               seamlessly. Check my projects here and my personal blogs here. if
