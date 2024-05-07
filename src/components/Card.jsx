8444a4207034d237202836b6b05e0a88e2e81150
Abdirahman Nur <myabdi4@gmail.com>
2024-05-07 17:14:13 -0500

    images
    change the directory for the images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 const Card = ({imageSource, projectTitle, projectDescription}) => {
     return (
         <>
-            <div className="card w-96 bg-base-100  image-full">
+            <div className="card w-96 bg-base-100  image-full cursor-pointer">
                 <figure><img src={imageSource} alt="project" className="w-full h-full object-fit"/></figure>
                 <div className="card-body">
                     <h2 className="card-title font-bold text-[#49df8f] uppercase">{projectTitle}</h2>
-                    <p className="text-white">{projectDescription}</p>
+                    <p className="text-white hover:text-[#b49595] hover:text-xl">{projectDescription}</p>
                     <div className="flex gap-2 justify-end pt-4">
                         <button className="btn hover-nav">GET CODE</button>
                         <button className="btn hover-nav">TRY DEMO</button>
