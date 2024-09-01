88a9658cc7219c639b81f8bf757dd462c65ebc6f
Abdirahman Nur <myabdi4@gmail.com>
2024-09-01 01:25:42 -0500

    Update HeroSection.jsx

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import "./HeroSection.CSS";
+import "./Herosection.css";
 import hero_banner from "../../assets/hero_banner.jpg";
+
+// COMPONENTS
 
 const HeroSection = () => {
   return (
     <div className="hero-section">
       <img src={hero_banner} alt="" className="hero_img" />
-      <websiteUnderMaintance/>
     </div>
   );
 };
