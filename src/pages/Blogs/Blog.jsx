ff6fec12324b3f4e183a3304803d8435c96c2190
Abdirahman Nur <myabdi4@gmail.com>
2024-09-07 15:32:21 -0500

    Update Blog.jsx

--- src/pages/Blogs/Blog.jsx
+++ src/pages/Blogs/Blog.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import ComingSoon from "../../components/ComingSoon";
 
 const Blog = () => {
   return (
     <div className="blogs">
       <Navbar />
-      <ComingSoon />
+      
     </div>
   );
 };
