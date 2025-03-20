49f40a69910b8665300f9b21c2fe38d986cc6b3c
Abdirahman Nur <myabdi4@gmail.com>
2025-03-20 02:45:17 -0500

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -8,12 +8,7 @@ const app = express();
 const port = 5000;
 
 // Enable CORS for the frontend to make requests to this server
-app.use(
-  cors({
-    origin: "https://nurabdirahman.vercel.app", // Replace this with your Vercel URL
-    methods: "GET, POST", // Specify allowed methods
-  })
-);
+app.use(cors());
 
 // Endpoint to get articles based on category
 app.get("/api/articles", async (req, res) => {
